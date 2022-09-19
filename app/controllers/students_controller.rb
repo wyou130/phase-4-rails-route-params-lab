f0e69a8568be2b79618b35ac44cf5eacb8ee70ca
Wendy You <wyou130@gmail.com>
2022-09-19 16:56:12 -0400

    commit tree 7d979a0d5f47c913b27b7e08d593252963c910db

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # byebug
+    # params[:name] ? 
+      # render json: Student.where(first_name: params[:name]) 
+      # || render json: Student.where(last_name: params[:name]) 
+    # : 
+      # render json: Student.all
+      if params[:name]
+        students = Student.select{|s| s.first_name.downcase == params[:name].downcase || s.last_name.downcase == params[:name].downcase }
+      else 
+        students = Student.all
+      end
+      render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
